7a1843622867994a9dda2d43e57bba941e446352
Jason Camacho <jasoncamacho@Jasons-MBP.attlocal.net>
2019-07-30 22:33:46 -0500

    Second Commit

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-const NumberButton = () => {
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
-  );
+const NumberButton = props => {
+  return 
+    <button onClick={() => props.setDisplay(
+      props.display === 0 ?
+        props.number:`${props.display}${props.number}`
+    )}>
+    {props.number}> 
+      {
+        /* Display a button element rendering the data being passed down from the parent container on props */}
+    </button>
+  
 };
+
+export default NumberButton
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const SpecialButton = () => {
-  return (
-    <>
+const SpecialButton = props => {
+  return  <button
+  style={{ gridArea: cn(props.special) }}
+  onClick={() => props.setDisplay(0)}>{props.special}</button>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
-  );
+    
+
 };
+
+export default SpecialButton
